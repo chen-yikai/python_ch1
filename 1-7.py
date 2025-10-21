d59c136cccd00d36999a7e6f9dca1aed057335f7
Yi Kai <s11304127@school.saihs.edu.tw>
2025-10-21 15:36:48 +0800

    20251021

--- 1-7.py
+++ 1-7.py
@@ -31,3 +31,42 @@ for i in range(6):
     for j in range(6-i):
         print("A",end="")
     print()
+#%% 九九乘法表
+for i in range(9):
+    i +=1
+    for j in range(9):
+        j+=1
+        print(f"{i} * {j} = {i*j}",end="\t")
+    print("")
+#%% 身分證字號
+id=input("請輸入身分證字號: ")
+for char in id:
+    for i in range(9):
+        if char == str(i):
+            print(char,end="")
+print("是你的數字部分")
+#%% Password
+right_password="123"
+while True:
+    user=input("請輸入密碼: ")
+    if user == right_password:
+        print("密碼正確!")
+        break;
+    else:
+        print("密碼錯誤!")
+#%% for...else
+text="Python"
+for char in text:
+    print(char)
+    #break;
+else:
+    print("輸出完成!")
+#%% timer
+import time
+
+count=10
+while count > 0:
+    print(count)
+    count-=1
+    time.sleep(1)
+print("時間到!")
