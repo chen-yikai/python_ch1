7886369fdb6597776700fc6c8b315b8c427c1dae
Elias Chen <eliaschen.dev@gmail.com>
2025-11-04 15:52:03 +0800

    20251104

--- 2-1.py
+++ 2-1.py
@@ -1,4 +1,27 @@
 #%% array
 list1=["meow",10,True,0.555]
 print(list1[0])
+list1[0]=19
+print(list1[1])
 print(list1[-1])
+for i in list1:
+    print(i)
+#%% user input
+length=int(input("請輸入資料個數: "))
+nums=[]
+for i in range(length):
+    current_input=int(input(f"請輸入第{i+1}個: "))
+    nums.append(current_input)
+print("你輸入的值依序為: ",end="")
+nums_sum=0
+for i in nums:
+    nums_sum+=i
+    print(i,end="\t")
+avg=nums_sum/length
+print(f"\n此串列數值總合為: {nums_sum}")
+print(f"此串列數值平均為: {avg:.2f}")
+print(f"串列中最大值{max(nums)}，最小值為{min(nums)}")
+print("此字串資料由大到小排序後: ",end="")
+nums.sort(reverse=True)
+for i in nums:
+    print(i,end="\t")
