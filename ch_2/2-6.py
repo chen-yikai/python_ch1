92fcdb93708e071691c793324891ef07f10bce47
elias <eliaschen.dev@gmail.com>
2026-01-09 23:14:37 +0800

    updatre

--- ch_2/2-6.py
+++ ch_2/2-6.py
@@ -1,4 +1,4 @@
-# %% Before excute
+# %% Before execute
 import matplotlib.pyplot as plt # 匯入繪圖模組
 
 font = {"family": "DFKai-SB"} # 設定中文字型
@@ -27,7 +27,7 @@ plt.plot(x, y, color="c", lw=2, ls="--", label="iPhone歷年銷售量", marker="
 plt.xlim(2016,2023) # x軸最大、最小值
 plt.ylim(0,100000) # y軸最大、最小值
 plt.grid(True) # 顯示格線
-plt.title("手機歷年銷售量") # 標題
+plt.title("手機歷年銷售量") # 圖表標題
 plt.xlabel("年度") # x軸標題
 plt.ylabel("銷售量") # y軸標題
 plt.legend()
@@ -40,12 +40,12 @@ lines=[[iPhone,"IPhone",'b','o','-'],[asus,"ASUS",'r','*','-.'],[mi,"小米",'g'
 # 繪製listX與listY的折線圖，設定顏色、線寬、線型、標籤、標記形狀及大小
 for item in lines:
     plt.plot(year, item[0], color=item[2], lw=2, ls=item[4], label=item[1], marker=item[3], ms=10)
-plt.xlim(2016,2023) # x軸最大、最小值
-plt.ylim(0,100000) # y軸最大、最小值
-plt.grid(True) # 顯示格線
-plt.title("手機歷年銷售量") # 標題
-plt.xlabel("年度") # x軸標題
-plt.ylabel("銷售量") # y軸標題
+plt.xlim(2016,2023) 
+plt.ylim(0,100000) 
+plt.grid(True) 
+plt.title("手機歷年銷售量") 
+plt.xlabel("年度") 
+plt.ylabel("銷售量") 
 plt.legend()
 #%% 長條圖
 year=[2017,2018,2019,2020,2021,2022]
@@ -65,24 +65,25 @@ asus=[23000,36000,40500,58000,65000,44000]
 mi=[13000,26000,50500,68000,75000,54000]
 # 繪製listX與listY的折線圖，設定顏色、線寬、線型、標籤、標記形狀及大小
 plt.bar(year,iPhone,label="IPhone")
-plt.bar(year,asus,label="IPhone",bottom=iPhone)
+plt.bar(year,asus,label="ASUS",bottom=iPhone)
 mi_bottom=[]
-for i in range(6):
+for i in range(6): # 計算小米長條圖的底部位置
     mi_bottom.append(iPhone[i]+asus[i])
 plt.bar(year,mi,label="小米",bottom=mi_bottom)
     
-plt.xlim(2016,2023) # x軸最大、最小值
-plt.ylim(0,300000) # y軸最大、最小值
-plt.grid(True) # 顯示格線
-plt.title("手機歷年銷售量") # 標題
-plt.xlabel("年度") # x軸標題
-plt.ylabel("銷售量") # y軸標題
+plt.xlim(2016,2023) 
+plt.ylim(0,300000)
+plt.grid(True) 
+plt.title("手機歷年銷售量") 
+plt.xlabel("年度") 
+plt.ylabel("銷售量") 
 plt.legend()
 #%% 圓餅圖
 listPrecent=[15.5,18,34.5,7,25]
 listBooks=['七龍珠','火影人者','航海王','第一神拳','多啦A夢']
 listColor=['red','green','pink','purple','yellow']
 listExplode=(0,0,0.2,0,0.1)
+# 繪製圓餅圖 (標籤，顏色，突起部分，標籤距圓心距離，百分比顯示，百分比距圓心距離，起始角度，陰影)
 plt.pie(listPrecent,labels=listBooks,colors=listColor,explode=listExplode,labeldistance=1.1,autopct='%3.1f%%',pctdistance=0.8,startangle=180,shadow=True)
-plt.axis("equal")
-plt.legend(loc="center left")
+plt.axis("equal") # 使圓餅圖為正圓形
+plt.legend(loc="center left") # 自訂圖例位置
